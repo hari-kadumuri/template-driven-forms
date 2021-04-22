8765efe69aaaf634bdd594864e60d28c79399ad5
Hari Prasad Raju Kadumuri <hari.kadumuri@gmail.com>
2021-04-22 09:26:45 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from "./enrollment.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-forms';
+  topics = ['Angular', 'React', 'Vue'];
+  topicHasError: boolean = true;
+  submitted = false;
+
+  userModel = new User('Rob', 'rob@test.com', 1234567890, 'default', 'morning', true);
+
+  constructor(private _enrollmentService: EnrollmentService) {
+
+  }
+
+  validateTopic(value: string) {
+    if(value == "default")  {
+      this.topicHasError = true;
+    }
+    else
+      this.topicHasError = false;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.userModel);
+    this._enrollmentService.enroll(this.userModel)
+      .subscribe(
+        data => console.log('Success! ', data),
+        error => console.log('Error! ', error)
+      )
+  }
+
 }
